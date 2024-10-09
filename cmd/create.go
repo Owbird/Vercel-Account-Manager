b4382bd37b939103b24aa694770a179f6eb8c6f2
owbird <owbirdphorccuo@gmail.com>
2024-10-09 13:35:13 +0000

    feat: Add accounts

---
+++ cmd/create.go
@@ -0,0 +1,85 @@
+package cmd
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/muesli/go-app-paths"
+
+	"github.com/spf13/cobra"
+)
+
+func copyFile(src string, dst string) {
+	data, err := os.ReadFile(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile(dst, data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Add a new account",
+	Long:  `Add an existing vercel account to the containers and can be checkout later`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+
+		vercelScope := gap.NewScope(gap.User, "com.vercel.cli")
+		vercelDirs, err := vercelScope.DataDirs()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		vercelDir := vercelDirs[0]
+
+		vamScope := gap.NewScope(gap.User, "vam")
+		vamDirs, err := vamScope.DataDirs()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		vamDir := vamDirs[0]
+
+		vCmd := exec.Command("vercel", "login", args[0])
+
+		vCmd.Stdout = os.Stdout
+		vCmd.Stderr = os.Stderr
+
+		vCmd.Run()
+		vCmd.Wait()
+
+		authPath := filepath.Join(vercelDir, "auth.json")
+		configPath := filepath.Join(vercelDir, "config.json")
+
+		accountDir := filepath.Join(vamDir, args[0])
+
+		os.MkdirAll(accountDir, os.ModePerm)
+
+		copyFile(configPath, filepath.Join(accountDir, "config.json"))
+		copyFile(authPath, filepath.Join(accountDir, "auth.json"))
+
+		log.Printf("[+] Account %s added", args[0])
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -10,21 +6,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "vercel-account-manager",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "vam",
+	Short: "Vercel CLI account manager",
+	Long:  `Seamlessly manage your vercel accounts`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	// Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +36,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
